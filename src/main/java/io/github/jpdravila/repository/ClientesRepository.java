6cadb23f94ad9374a3690babc570b894f408e2a9
João Pedro de Ávila <jpdravila@gmail.com>
2022-05-30 06:01:47 -0400

    Injeção de dependência

---
+++ src/main/java/io/github/jpdravila/repository/ClientesRepository.java
@@ -0,0 +1,13 @@
+package io.github.jpdravila.repository;
+
+import io.github.jpdravila.model.Cliente;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class ClientesRepository {
+
+    public void persistir(Cliente cliente){
+        //acessa base de dados e salva o cliente
+    }
+
+}
--- src/main/java/io/github/jpdravila/repository/MeuRepository.java
+++
@@ -1,7 +0,0 @@
-package io.github.jpdravila.repository;
-
-import org.springframework.stereotype.Repository;
-
-@Repository
-public class MeuRepository {
-}

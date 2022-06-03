b52bdd69c32181e3917df2bf7e75ae1719b15e28
João Pedro de Ávila <jpdravila@gmail.com>
2022-06-02 20:04:44 -0400

    Criando classes de modelo

---
+++ src/main/java/io/github/jpdravila/VendasApplication.java
@@ -0,0 +1,4 @@
+package io.github.jpdravila;
+
+public class VendasApplication {
+}
---
+++ src/main/java/io/github/jpdravila/domain/entity/Produto.java
@@ -0,0 +1,9 @@
+package io.github.jpdravila.domain.entity;
+
+import java.math.BigDecimal;
+
+public class Produto {
+    private Integer id;
+    private String descricao;
+    private BigDecimal preco;
+}

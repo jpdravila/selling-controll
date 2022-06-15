30f7610c4a13ad9ceca0c724dc65963e08007d7a
João Pedro de Ávila <jpdravila@gmail.com>
2022-06-15 05:30:55 -0400

    Mapeando a entidade pedido

--- src/main/java/io/github/jpdravila/domain/entity/Cliente.java
+++ src/main/java/io/github/jpdravila/domain/entity/Cliente.java
@@ -1,6 +1,7 @@
 package io.github.jpdravila.domain.entity;
 
 import javax.persistence.*;
+import java.util.Set;
 
 @Entity
 @Table(name = "cliente")
@@ -13,6 +14,9 @@ public class Cliente {
 
     @Column(name = "nome", length = 100)
     private String nome;
+
+    @OneToMany(mappedBy = "cliente")
+    private Set<Pedido> pedidos;
 
     public Cliente() {
     }

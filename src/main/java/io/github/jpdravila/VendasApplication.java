e7a95ad5f101304cc14ed934940714053a381a15
João Pedro de Ávila <jpdravila@gmail.com>
2022-06-17 13:18:34 -0400

    Requisicao GET com parâmetros e ResponseEntity

--- src/main/java/io/github/jpdravila/VendasApplication.java
+++ src/main/java/io/github/jpdravila/VendasApplication.java
@@ -1,10 +1,24 @@
 package io.github.jpdravila;
 
+import io.github.jpdravila.domain.entity.Cliente;
+import io.github.jpdravila.domain.repository.ClienteRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class VendasApplication {
+
+    @Bean
+    public CommandLineRunner execultar(@Autowired ClienteRepository clientes){
+        return args -> {
+            Cliente c = new Cliente(null, "Joao Pedro");
+            clientes.save(c);
+        };
+    }
+
     public static void main(String[] args){
         SpringApplication.run(VendasApplication.class, args);
     }

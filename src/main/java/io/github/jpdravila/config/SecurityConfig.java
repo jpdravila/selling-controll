d8e7e8abec4dc09c3db0e048c169989f60263177
João Pedro de Ávila <jpdravila@gmail.com>
2022-08-15 21:39:05 -0400

    Password encoder

--- src/main/java/io/github/jpdravila/config/SecurityConfig.java
+++ src/main/java/io/github/jpdravila/config/SecurityConfig.java
@@ -6,9 +6,15 @@ import org.springframework.security.config.annotation.web.WebSecurityConfigurer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    public PasswordEncoder passwordEncoder(){
+        return new BCryptPasswordEncoder();
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception{

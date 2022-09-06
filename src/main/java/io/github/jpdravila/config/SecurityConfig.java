a1e47945292a102afbab760ec1f6469425bba370
João Pedro de Ávila <jpdravila@gmail.com>
2022-09-05 21:43:29 -0400

    Implementacao do UserDatailsService

--- src/main/java/io/github/jpdravila/config/SecurityConfig.java
+++ src/main/java/io/github/jpdravila/config/SecurityConfig.java
@@ -1,6 +1,9 @@
 package io.github.jpdravila.config;
 
-import org.springframework.context.annotation.Configuration;
+import io.github.jpdravila.impl.UsuarioServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -8,34 +11,37 @@ import org.springframework.security.config.annotation.web.configuration.WebSecur
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
-@Configuration
 @EnableWebSecurity
-public class SecurityConfig extends WebSecurityConfigurerAdapter{
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
+    @Autowired
+    @Lazy
+    private UsuarioServiceImpl usuarioService;
+
+    @Bean
     public PasswordEncoder passwordEncoder(){
         return new BCryptPasswordEncoder();
     }
 
     @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception{
-        auth.inMemoryAuthentication().passwordEncoder(passwordEncoder())
-                .withUser("fulano")
-                .password(passwordEncoder().encode("123"))
-                .roles("USER", "ADMIN");
-    }
-    @Override
-    protected void configure(HttpSecurity http) throws Exception{
-        http
-                .csrf().disable()
-                .authorizeRequests()
-                .antMatchers("/api/clientes/**")
-                .hasAnyRole("USER", "ADMIN")
-                .antMatchers("/api/produtos/**")
-                .hasRole("ADMIN").
-                antMatchers("/api/pedidos")
-                .hasAnyRole("USER", "ADMIN")
-                .and()
-                .httpBasic();
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(usuarioService)
+                .passwordEncoder(passwordEncoder());
     }
 
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+            http
+                    .csrf().disable()
+                    .authorizeRequests()
+                    .antMatchers("/api/clientes/**")
+                    .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/pedidos/**")
+                    .hasAnyRole("USER", "ADMIN")
+                    .antMatchers("/api/produtos/**")
+                    .hasRole("ADMIN")
+                    .and()
+                    .httpBasic();
+            ;
+    }
 }
